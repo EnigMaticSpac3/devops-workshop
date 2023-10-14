f48207020888122bd715d22b8ee5a75feadbfeef
enigmaticspac3 <jgonzreal62@gmail.com>
2023-10-14 10:37:12 -0500

    deploy to githubpages

--- counter.js
+++ counter.js
@@ -1,8 +1,10 @@
+import Even from 'is-even'
+
 export function setupCounter(element) {
   let counter = 0
   const setCounter = (count) => {
     counter = count
-    element.innerHTML = `count is ${counter}`
+    element.innerHTML = `count is ${counter} | El numero es ${Even(counter) ? 'par' : 'impar'}`
   }
   element.addEventListener('click', () => setCounter(counter + 1))
   setCounter(0)
